1cf9430148183fef63d57e114ddcd0edee72b610
Nhi Nguyen <nguyenphanthaonhi@gmail.com>
2017-11-03 14:44:22 +0700

    Add files via upload

---
+++ DispatchServlet.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sampie.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author admin
+ */
+public class DispatchServlet extends HttpServlet {
+    private final String loginPage = "login.html";
+    private final String loginServlet = "Login2Servlet";
+     private final String searchLastnameServlet = "SearchLastnameServlet";
+     private final String deleteAcountServlet = "DeleteAcountServlet";
+     private final String updatePassRoleServlet = "UpdatePassRoleServlet";
+     private final String nullServlet = "NullServlet";
+     private final String logoutServlet = "LogoutServlet";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String url = loginPage;
+        try {
+           String button = request.getParameter("btAction");
+           
+           
+          if(button == null){   
+              url = nullServlet;
+          }else if(button.equals("Login")){
+              url = loginServlet;
+          }else if (button.equals("Search")){
+              url = searchLastnameServlet;
+          }else if(button.equals("delete")){
+              url = deleteAcountServlet;
+          }else if(button.equals("Update")){
+              url = updatePassRoleServlet;
+          }else if(button.equals("Logout")){
+              url = logoutServlet;
+          }
+        }finally{
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
+            out.close();
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
